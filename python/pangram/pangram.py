2e6930353efa8101e4b43201da1a51f3d8ed0221
MikiCyg <miki@comsite.com.pl>
2018-09-01 23:37:08 +0200

    Pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,8 @@
 def is_pangram(sentence):
-    pass
+    all_letters = 'abcdefghijklmnopqrstuvwxyz'
+    sentence_without_whitespace_lowercase=sentence.lower().replace(" ", "")
+    for x in sentence:
+        if x not in all_letters:
+            return False
+    return True
+
